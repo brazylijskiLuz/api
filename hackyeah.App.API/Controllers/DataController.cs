b0ce8f03ecec18ae31b235a66170140941399a9a
Jan Rzepisko <60238932+JanRzepisko@users.noreply.github.com>
2023-09-30 20:02:30 +0200

    query algorithm

--- hackyeah.App.API/Controllers/DataController.cs
+++ hackyeah.App.API/Controllers/DataController.cs
@@ -7,14 +7,16 @@ using Shared.BaseModels.ApiControllerModels;
 namespace hackyeah.App.API.Controllers;
 
 [ApiController]
-[Route("/api")]
+[Route("/api/[controller]")]
 public class UniversityController : BaseApiController
 {
     public UniversityController(IMediator mediator) : base(mediator) { }
 
     [HttpGet]
+    [Route("query")]
     public Task<IActionResult> Get(string query, int page) => Endpoint(new GetByQuery.Command(query, page));
     [HttpGet]
+    [Route("by-city")]
     public Task<IActionResult> GetByCity(Guid cityId, int page) => Endpoint(new GetByCity.Command(cityId, page));
 }
 
--- hackyeah.App.Infrastructure/Repository/InstitutionRepository.cs
+++ hackyeah.App.Infrastructure/Repository/InstitutionRepository.cs
@@ -21,27 +21,52 @@ public class InstitutionRepository<T> : BaseRepository<T>, IInstitutionRepositor
 
     public Task<List<T>> GetByQueryAsync(string query, int page, int pageSize, CancellationToken cancellationToken)
     {
-        string sql = "SELECT * FROM _universityData Where InstitutionName Like ";
-        var charList = query.ToCharArray();
+        string sql = "SELECT * FROM \"_universityData\" WHERE ";
+        var charList = query.Split(' ').ToList();
+        int i = 0;
+
+        sql += "(";
+        
         foreach (var c in charList)
         {
-            sql += $"%{c}% OR InstitutionName Like ";
-        }
-        foreach (var c in charList)
-        {
-            sql += $"%{c}% OR Website Like ";
-        }        
-        foreach (var c in charList)
-        {
-            sql += $"%{c}% OR Address_City Like ";
+            if (i != 0)
+                sql += $" AND \"InstitutionName\" Like '%{c}%' ";
+            else
+                sql += $" \"InstitutionName\" Like '%{c}%' ";
+            i++;
+
         }
 
-        sql += ";";
-        
-        return _entities
-            .FromSqlRaw(sql)
+        i = 0;
+        sql += ") OR (";
+
+        foreach (var c in charList)
+        {
+            if (i != 0)
+                sql += $" AND \"Website\" Like '%{c}%' ";
+            else
+                sql += $" \"Website\" Like '%{c}%' ";
+            i++;
+        }
+
+        i = 0;
+        sql += ") OR (";
+
+        foreach (var c in charList)
+        {
+            if (i != 0)
+                sql += $" AND \"Address_City\" Like '%{c}%'";
+            else
+                sql += $" Lower(\"Address_City\") Like Lower('%{c}%')";
+            i++;
+        }
+
+        sql += $") OFFSET  {page * pageSize} limit {pageSize}";
+
+        Console.WriteLine(sql);
+        return _entities.FromSqlRaw(sql)
             .Include(c => c.Directions)
-            .Skip(page * pageSize).Take(pageSize).ToListAsync<T>(cancellationToken: cancellationToken);
+            .ToListAsync<T>(cancellationToken: cancellationToken);
     }
 
     public Task<List<T>> GetByCityAsync(string city, int page, int pageSize, CancellationToken cancellationToken) => 
